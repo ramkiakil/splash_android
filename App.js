6850739e67356ef35ff768575e40397ed48fe21a
Ramki <rramki51@gmail.com>
2019-01-14 16:04:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import SplashScreen from 'react-native-smart-splash-screen'
+
+export default class App extends Component {
+  componentDidMount () {
+    //SplashScreen.close(SplashScreen.animationType.scale, 850, 500)
+    SplashScreen.close({
+       animationType: SplashScreen.animationType.scale,
+       duration: 850,
+       delay: 500,
+    })
+      }
+  render() {
+
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
